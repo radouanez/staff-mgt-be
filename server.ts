fac9e09de0c3a9f4f1c8a513131b91d7b2666175
ZAKRAOUI Radouane <radouane.zakraoui2@fr.ibm.com>
2020-05-01 12:51:59 +0200

    test gcp configuration

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
+  res.status(200).send('staff management service').end();
 });
 
 // Start the server
