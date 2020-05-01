d7deed6bc02485e015bfb1d6145b4c39b8a3678f
ZAKRAOUI Radouane <radouane.zakraoui2@fr.ibm.com>
2020-05-01 18:21:59 +0200

    test env configuration

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const nodeEnv = process.env.NODE_ENV;
-  const data = `staff management service in${nodeEnv}`;
+  const data = `staff management service in${process.env.DB_URI}`;
   res.status(200).send(data).end();
 });
 
